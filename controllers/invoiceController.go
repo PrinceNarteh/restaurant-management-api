2ad1d4ce3984534a9a149882b4e1848d8f68ecb3
PrinceNarteh <prinarteh@gmail.com>
2022-01-16 18:03:54 +0000

    add invoice routes

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,1 +1,19 @@
 package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+func GetInvoices(c *fiber.Ctx) error {
+	return c.SendString("All Invoices")
+}
+
+func GetInvoice(c *fiber.Ctx) error {
+	return c.SendString("Get Single Invoice")
+}
+
+func CreateInvoice(c *fiber.Ctx) error {
+	return c.SendString("Create Invoice")
+}
+
+func UpdateInvoice(c *fiber.Ctx) error {
+	return c.SendString("Update Invoice")
+}
--- routes/invoiceRouter.go
+++ routes/invoiceRouter.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/PrinceNarteh/restaurant-management-api/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func InvoiceRoutes(router fiber.Router) {
+	invoiceRouter := router.Group("/Invoices")
+	invoiceRouter.Get("/", controllers.GetInvoices)
+	invoiceRouter.Get("/:InvoiceId", controllers.GetInvoice)
+	invoiceRouter.Post("/", controllers.CreateInvoice)
+	invoiceRouter.Patch("/:InvoiceId", controllers.UpdateInvoice)
+}
