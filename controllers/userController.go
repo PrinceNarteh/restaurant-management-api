7f7fbbefb5ee4213b28474c24514247095c62e07
PrinceNarteh <prinarteh@gmail.com>
2022-01-16 17:44:06 +0000

    add users routes

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,19 @@
 package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+func GetUser(ctx *fiber.Ctx) error {
+	return ctx.SendString("Get User")
+}
+
+func GetUsers(ctx *fiber.Ctx) error {
+	return ctx.SendString("Get Users")
+}
+
+func Register(ctx *fiber.Ctx) error {
+	return ctx.SendString("Register")
+}
+
+func Login(ctx *fiber.Ctx) error {
+	return ctx.SendString("Get Users")
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	controller "github.com/PrinceNarteh/restaurant-management-api/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func UserRoutes(incomingRoute fiber.Router) {
+	router := incomingRoute.Group("/users")
+	router.Get("/", controller.GetUsers)
+	router.Get("/:userId", controller.GetUser)
+	router.Post("/register", controller.Register)
+	router.Post("/login", controller.Login)
+}
