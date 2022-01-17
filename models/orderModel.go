72442af53552f33da752f9331c009f997813a3b9
PrinceNarteh <prinarteh@gmail.com>
2022-01-17 02:18:15 +0000

    adds Order model

--- models/orderModel.go
+++ models/orderModel.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Order struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	CreatedAt time.Time          `json:"createdAt"`
+	UpdatedAt time.Time          `json:"updatedAt"`
+	OrderId   string             `json:"orderId"`
+	OrderDate time.Time          `json:"orderDate" validate:"required"`
+	TableId   *string            `json:"tableId" validate:"required"`
+}
