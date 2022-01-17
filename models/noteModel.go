87f44dcae5161d0f517a38f9744c2feff4c811a5
PrinceNarteh <prinarteh@gmail.com>
2022-01-17 02:42:53 +0000

    adds Note model

--- models/noteModel.go
+++ models/noteModel.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Note struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	CreatedAt time.Time          `json:"createdAt"`
+	UpdatedAt time.Time          `json:"updatedAt"`
+	UserId    string             `json:"userId"`
+	Title     string             `json:"title"`
+	Text      string             `json:"text"`
+}
