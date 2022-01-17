cd791a4e2d2275d2954b33e570e448e7089b7279
PrinceNarteh <prinarteh@gmail.com>
2022-01-17 02:40:24 +0000

    adds User model

--- models/userModel.go
+++ models/userModel.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID           primitive.ObjectID `bson:"_id"`
+	CreatedAt    time.Time          `json:"createdAt"`
+	UpdatedAt    time.Time          `json:"updatedAt"`
+	UserId       string             `json:"userId"`
+	FirstName    *string            `json:"firstName" validate:"required,min=2,max=100"`
+	LastName     *string            `json:"lastName" validate:"required,min=2,max=100"`
+	Email        *string            `json:"email" validate:"required,email"`
+	Password     *string            `json:"password" validate:"required,min=6"`
+	Avatar       *string            `json:"avatar" validate:"required"`
+	Phone        *string            `json:"phone"`
+	AccessToken  *string            `json:"accessToken"`
+	RefreshToken *string            `json:"refreshToken"`
+}
