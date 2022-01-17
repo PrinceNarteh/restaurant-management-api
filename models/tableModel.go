4ebca0ef92228aed4dfde4b8dddaa43e0bc92581
PrinceNarteh <prinarteh@gmail.com>
2022-01-17 02:13:29 +0000

    adds Table model

--- models/tableModel.go
+++ models/tableModel.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Table struct {
+	ID             primitive.ObjectID `bson:"_id"`
+	CreatedAt      time.Time          `json:"createdAt"`
+	UpdatedAt      time.Time          `json:"updatedAt"`
+	TableId        string             `json:"tableId"`
+	TableNumber    *int               `json:"tableNumber" validate:"required"`
+	NumberOfGuests *int               `json:"numberOfGuests"`
+}
