947a42faac0a907c570d21caa0da72572318854c
PrinceNarteh <prinarteh@gmail.com>
2022-01-21 15:33:18 +0000

    add GetMenus handler

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,9 +1,28 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"github.com/PrinceNarteh/restaurant-management-api/database"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"log"
+	"time"
+)
+
+var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
 
 func GetMenus(c *fiber.Ctx) error {
-	return c.SendString("All Menus")
+	result, err := database.MenuCollection.Find(ctx, bson.D{})
+	defer cancel()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "could not get menus"})
+	}
+	var menus bson.M
+	if err = result.All(ctx, &menus); err != nil {
+		log.Fatal(err)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"data": menus})
 }
 
 func GetMenu(c *fiber.Ctx) error {
