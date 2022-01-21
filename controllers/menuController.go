09c4daa85069ea1557ac50e392098534f75b6891
PrinceNarteh <prinarteh@gmail.com>
2022-01-21 18:48:48 +0000

    add UpdateMenu handler

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"time"
 )
@@ -61,5 +62,45 @@ func CreateMenu(c *fiber.Ctx) error {
 }
 
 func UpdateMenu(c *fiber.Ctx) error {
+	var menu models.Menu
+	if err := c.BodyParser(&menu); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "error parsing json"})
+	}
+
+	menuId := c.Params("menuId")
+	filter := bson.M{"menuId": menuId}
+	var updatedObj primitive.D
+
+	if menu.StartDate != nil && menu.EndDate != nil {
+		if !inTimeSpan(*menu.StartDate, *menu.EndDate, time.Now()) {
+			return c.Status(fiber.StatusInternalServerError).JSON(filter.Map{"error": "kindly check the data"})
+		}
+
+		updatedObj = append(updatedObj, bson.E{"startDate", menu.StartDate})
+		updatedObj = append(updatedObj, bson.E{"endDate", menu.EndDate})
+
+		if menu.Name != "" {
+			updatedObj = append(updatedObj, bson.E{"name", menu.Name})
+		}
+
+		if menu.Category != "" {
+			updatedObj = append(updatedObj, bson.E{"category", menu.Category})
+		}
+
+		menu.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updatedObj = append(updatedObj, bson.E{"updatedAt", menu.UpdatedAt})
+
+		upsert := true
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+		result, err := db.MenuCollection.UpdateOne(ctx, filter, bson.D{{"$set", updatedObj}}, &opt)
+		defer cancel()
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Menu update failed"})
+		}
+		return c.Status(fiber.StatusOK).JSON(result)
+	}
+
 	return c.SendString("Update Menu")
 }
